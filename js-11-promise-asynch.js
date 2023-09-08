bf0089dd48680cca095941d12f2e2b4bec6e71b9
digitrom <roma.bobrovski@gmail.com>
2023-09-09 01:29:41 +0200

    js-12 - callstack & callback queue work examples

--- js-11-promise-asynch.js
+++ js-11-promise-asynch.js
@@ -7,70 +7,68 @@
 //- then-catch - приемлимо
 //- asynch-await - предпочтительно
 //============================================= 1 =====================================================================
-// const server1 = {
-//     getData() {
-//         return new Promise((res, rej) => {
-//             setTimeout(() => {
-//                 res('Promise resolved') // если отрабатывает функция res, то промис резолвится переданными в него данными
-//             }, 4000)
-//         })
-//     }
-// }
-//
-// const pr1 = server1.getData()
-// console.log(pr1)
-//
-// const server2 = {
-//     getData() {
-//         return new Promise((res, rej) => {
-//             setTimeout(() => {
-//                 rej('Error') // если отрабатывает функция rej, то промис реджектится переданной ошибкой
-//             }, 2000)
-//         })
-//     }
-// }
-//
-// const pr2 = server2.getData()
+const server1 = {
+    getData() {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                res('Promise resolved') // если отрабатывает функция res, то промис резолвится переданными в него данными
+            }, 4000)
+        })
+    }
+}
 
-// pr2.then((data) => { // подписваемся на данные, которые получили и выводим их в консоль
-//     console.log('eeee', data )
-// }).catch(err => { // лучше всего для отлова ошибок использовать catch
-//     console.log('something went wrong', err)
-// })
-// .then(null, (err) => { // но можно отловить ошибку с then, используя 2-ой передаваемый колбэк, 1-ый при этом ставим null
-//     console.log('something went wrong', err)
-// })
+const pr1 = server1.getData()
+console.log(pr1)
 
-// pr1.then((data)=> {
-//     console.log('data',data) // "data Promise resolved"
-//     return 'some string' // "some string" - НЕ показывается в консоле, а передается по цепочке в then ниже
-// })
-//     .then((str) => { // в then попадает не объект промис, а то что он возвращает вышележащий then!
-//         console.log(str) //"some string"
-//         return str// передаем str ниже
-//     })
-//     .then((str) => {
-//         console.log(str)//"some string"
-//     })
-//     .then((str) => {
-//         console.log(str)//"some string"
-//         // если нету return у предыдущего then - возвращает undefined
-//     })
-//     .then((str)=> {
-//         console.log('data',sr) // если есть ошибка, то ниже ее then-ы пропусукаются и попадаем в catch
-//         // return fetch('https://google.com/?query=js')
-//         return str
-//     })
-//     .then((str) => {
-//         console.log(str)//"some string"
-//     })
-//     .catch((err)=> {
-//         console.log('ERROR:', err) // выводим ошибку "ERROR: sr is not defined"
-//     })
-//     .finally(()=> {
-//         console.log('Finally')
-//     })
+const server2 = {
+    getData() {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                rej('Error') // если отрабатывает функция rej, то промис реджектится переданной ошибкой
+            }, 2000)
+        })
+    }
+}
 
+const pr2 = server2.getData()
+pr2.then((data) => { // подписваемся на данные, которые получили и выводим их в консоль
+    console.log('eeee', data )
+}).catch(err => { // лучше всего для отлова ошибок использовать catch
+    console.log('something went wrong', err)
+})
+.then(null, (err) => { // но можно отловить ошибку с then, используя 2-ой передаваемый колбэк, 1-ый при этом ставим null
+    console.log('something went wrong', err)
+})
+
+pr1.then((data)=> {
+    console.log('data',data) // "data Promise resolved"
+    return 'some string' // "some string" - НЕ показывается в консоле, а передается по цепочке в then ниже
+})
+    .then((str) => { // в then попадает не объект промис, а то что он возвращает вышележащий then!
+        console.log(str) //"some string"
+        return str// передаем str ниже
+    })
+    .then((str) => {
+        console.log(str)//"some string"
+    })
+    .then((str) => {
+        console.log(str)//"some string"
+        // если нету return у предыдущего then - возвращает undefined
+    })
+    .then((str)=> {
+        console.log('data',sr) // если есть ошибка, то ниже ее then-ы пропусукаются и попадаем в catch
+        // return fetch('https://google.com/?query=js')
+        return str
+    })
+    .then((str) => {
+        console.log(str)//"some string"
+    })
+    .catch((err)=> {
+        console.log('ERROR:', err) // выводим ошибку "ERROR: sr is not defined"
+    })
+    .finally(()=> {
+        console.log('Finally')
+    })
 // в консоле мы увидим:
 
 // сначала объект Promise, который в сосстоянии "pending" - ожидании. Он не успел со старта зарезолвиться так как,
@@ -238,32 +236,31 @@ Promise.race([promise1, promise2, promise3]).then((data) => {// в then попа
 // т.к. этот промис заредлектился 1-ее всех
 
 //------------------------------------------------ allSettled ----------------------------------------------------------
-// allSettled всегда резолвится массивом объектов . в каждом объекте есть 2 св-ва:
-// -если промис заразолвился то св-ва будут status и value
-// -если промис rejected то св-ва будут status и reason
-// const promise1 = fetch('https://google.com/?query=js')
-// const promise2 = fetch('https://yah12345oo.com/?query=js')
-// const promise3 = fetch('https://duckduckgo.com/?query=js')
-//
-// Promise.allSettled([promise1, promise2, promise3]).then((data) => {
-//     console.log(data)
-// })
-// //в консоли будет типа того:
-//     [{
-//     status: fulfilled,
-//     value: Response
-// },
-//     {
-//         status: fulfilled,
-//         reason: TypeEror
-//     },
-//     {
-//         status: fulfilled,
-//         value: Response
-//     }
-//     ]
-// catch в allSettled не отрабатывает - в нем нет смысла
+allSettled всегда резолвится массивом объектов . в каждом объекте есть 2 св-ва:
+-если промис заразолвился то св-ва будут status и value
+-если промис rejected то св-ва будут status и reason
+const promise1 = fetch('https://google.com/?query=js')
+const promise2 = fetch('https://yah12345oo.com/?query=js')
+const promise3 = fetch('https://duckduckgo.com/?query=js')
 
+Promise.allSettled([promise1, promise2, promise3]).then((data) => {
+    console.log(data)
+})
+//в консоли будет типа того:
+    [{
+    status: fulfilled,
+    value: Response
+},
+    {
+        status: fulfilled,
+        reason: TypeEror
+    },
+    {
+        status: fulfilled,
+        value: Response
+    }
+    ]
+catch в allSettled не отрабатывает - в нем нет смысла
 //----------------------------------------------------- any ------------------------------------------------------------
 
 // any ждет ответа от всех промисов и
